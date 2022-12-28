73f1210d98aba4975d7e9d78abca1d20a040d7ac
Diogo Leite <diogoleite87@gmail.com>
2022-12-28 19:15:53 -0300

    feat: react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './router'
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   )
 }
 
