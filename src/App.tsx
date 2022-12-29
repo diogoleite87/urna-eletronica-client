d141cd6680c68406962a49f450c5daae84a0223f
Diogo Leite <diogoleite87@gmail.com>
2022-12-29 20:45:48 -0300

    feat: add global styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './router'
+import GlobalStyled from './styles/global'
 
 function App() {
 
   return (
     <BrowserRouter>
+      <GlobalStyled />
       <Router />
     </BrowserRouter>
   )
