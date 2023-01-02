10cb0f3ae0a03c10bdc3a824ab50a8722b358557
Diogo Leite <diogoleite87@gmail.com>
2023-01-02 13:54:47 -0300

    feat: state represenative page

---
+++ src/pages/StateRepresentative/index.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { Container } from "./style";
+import KeyBoard from "../../components/KeyBoard";
+import InputCandidate from "../../components/InputCandidate";
+
+let inputs = [
+    { number: "" },
+    { number: "" },
+    { number: "" },
+    { number: "" },
+    { number: "" },
+]
+
+export default function StateRepresentative() {
+
+    const [number, setNumber] = useState<string>("");
+    const [name, setName] = useState<string>("");
+    const [part, setPart] = useState<string>("");
+
+    useEffect(() => {
+
+        for (let index = 0; index < inputs.length; index++) {
+            inputs[index].number = number[index]
+        }
+
+    }, [number])
+
+    return (
+        <Container>
+            <InputCandidate candidateType={"Deputado Estadual"} name={name} part={part} inputs={inputs} />
+            <KeyBoard state={number} setState={setNumber} />
+        </Container>
+    )
+}
