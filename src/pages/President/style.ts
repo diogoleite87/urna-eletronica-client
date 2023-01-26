4fde58cad014f0d68022092f19cb1bfc35e1814f
Diogo Leite <diogoleite87@gmail.com>
2023-01-26 00:29:48 -0300

    feat: styles for president page

--- src/pages/President/style.ts
+++ src/pages/President/style.ts
@@ -7,3 +7,65 @@ export const Container = styled.div`
     align-items: center;
     margin: 3vh;
 `
+
+export const ContainerScreen = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 25px;
+    width: 785px;
+    height: 482px;
+    left: 88px;
+    top: 299px;
+`
+
+export const ContainerNumber = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 0px;
+    gap: 16px;
+
+    width: 749px;
+    height: 157px;
+`
+
+export const BoxNumber = styled.p`
+    width: 70%;
+    height: 70%;
+    border: 3px solid #565656;
+    font-size: 5em;
+    text-align: center;
+`
+
+export const TextPart = styled.div`
+    font-size: 3em;
+    width: 100%;
+`
+
+export const TextCondidateType = styled.div`
+    display: flex;
+    align-items: center;
+    tet-align: center;
+    justify-content: center;
+    font-size: 3em;
+    width: 100%;
+`
+export const ContainerImage = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    margin: 3vh;
+`
+
+export const ImageCandidate = styled.img`
+
+`
+
+export const Gif = styled.img`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
