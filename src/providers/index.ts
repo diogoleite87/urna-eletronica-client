fe2fc44b445b91b4ed9172b87d0e862b1bcb8ab3
Diogo Leite <diogoleite87@gmail.com>
2023-01-26 15:07:46 -0300

    fix: baseURL

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const Api = axios.create({
-    baseURL: 'http://0.0.0.0:8000/api',
+    baseURL: 'http://localhost:8000/api',
     headers: { 'Content-Type': 'application/json' }
 })
