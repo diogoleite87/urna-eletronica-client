a55ba8c72ec1983b963e43820f0654b03ca7371f
Diogo Leite <diogoleite87@gmail.com>
2023-01-26 13:20:35 -0300

    feat: add type Candidate

---
+++ src/schemas/Models.ts
@@ -0,0 +1,9 @@
+export type Candidate = {
+    number: string,
+    uf: string,
+    picture: string,
+    name: string,
+    substituteName: string,
+    substitutePicture: string,
+    partyNumber: string
+}
