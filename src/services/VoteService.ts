7b9a14d1fc1d46e55dc4e16a8b7d41021e62d824
Diogo Leite <diogoleite87@gmail.com>
2023-03-21 19:48:18 -0300

    fix: update person hasVoted true

--- src/services/VoteService.ts
+++ src/services/VoteService.ts
@@ -2,7 +2,7 @@ import { Api } from "../providers";
 import { PersonVotedDTO, VoteDTO } from "../schemas";
 
 const postVote = (vote: VoteDTO) => Api.post(`/vote`, vote)
-const hasVoted = (hasVoted: PersonVotedDTO, cpf: string) => Api.put(`person?cpf=${cpf}`)
+const hasVoted = (hasVoted: PersonVotedDTO, cpf: string) => Api.put(`person?cpf=${cpf}`, hasVoted)
 
 export const VoteService = {
     postVote,
